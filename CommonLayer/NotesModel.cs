e846683e97107e0a0337448234e87f82b9f5c85b
Prabal <prabal760@gmail.com>
2021-09-14 11:55:31 +0530

    [Prabal] Added-Login Model is implemented

---
+++ CommonLayer/NotesModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+   public class NotesModel
+    {
+        public long UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public string Reminder { get; set; }
+
+
+        public string Color { get; set; }
+
+        public string image { get; set; }
+
+        public bool IsArchive { get; set; }
+
+
+
+        public bool IsTrash;
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? ModifiedAt { get; set; }
+
+    }
+}
