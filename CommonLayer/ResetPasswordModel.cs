cfa6348fc052782028d1256ea0f2e88e8a2429d1
Prabal <prabal760@gmail.com>
2021-09-18 16:57:27 +0530

    [Prabal] Added-Reset password model.

---
+++ CommonLayer/ResetPasswordModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer
+{
+   public class ResetPasswordModel
+    {
+
+      
+        
+        
+        public string Password { get; set; }
+
+       
+        [Display(Name="Confirm Password")]
+        [Compare("Password",ErrorMessage="Password and Confirm Password must match")]
+        public string ConfirmPassword { get; set; }
+
+      
+
+
+    }
+}
