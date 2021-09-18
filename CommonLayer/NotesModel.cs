c753f1e8c6af132f92d6373a6c165407f72379b8
Prabal <prabal760@gmail.com>
2021-09-18 16:55:03 +0530

    [Prabal] Refactored-isPin property is added.

--- CommonLayer/NotesModel.cs
+++ CommonLayer/NotesModel.cs
@@ -6,7 +6,7 @@ namespace CommonLayer
 {
    public class NotesModel
     {
-        public long UserId { get; set; }
+        
 
         public string Title { get; set; }
 
@@ -14,16 +14,16 @@ namespace CommonLayer
 
         public string Reminder { get; set; }
 
-
         public string Color { get; set; }
 
         public string image { get; set; }
 
         public bool IsArchive { get; set; }
 
+        public bool isPin { get; set; }
 
+        public bool IsTrash { get; set; }
 
-        public bool IsTrash;
         public DateTime CreatedAt { get; set; }
 
         public DateTime? ModifiedAt { get; set; }
