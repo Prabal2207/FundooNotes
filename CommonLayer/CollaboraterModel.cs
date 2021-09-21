f5e7f12cc080fee29dad0b4d33d204b15a54edf3
Prabal <prabal760@gmail.com>
2021-09-21 09:56:45 +0530

    [Prabal]Added-Collaborator Model.

---
+++ CommonLayer/CollaboraterModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class CollaboraterModel
+    {
+        public string CollaboratorEmailId { get; set; }
+
+    }
+}
