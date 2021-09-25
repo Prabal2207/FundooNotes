ddaff7c820f2c7fff3cad6a9f96f8c743fc4f54a
Prabal <prabal760@gmail.com>
2021-09-25 11:10:26 +0530

    [Prabal] Added-Label repository interface

---
+++ RepositoryLayer/Interface/ILabelRL.cs
@@ -0,0 +1,27 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+   public interface ILabelRL
+    {
+        bool AddLabeltoNotes(LabelModel labelmodel, long Noteid, long userid);
+
+        bool AddLabeltoUser(LabelModel labelmodel, long userid);
+
+        bool EditLabel(LabelModel labelmodel);
+
+        bool DeleteLabel(long Labelid, long userid);
+
+        bool RemoveLabel(long Labelid, long userid, long noteid);
+
+        List<Label> GetNoteLables(long noteId, long userId);
+
+        List<Label> GetUserLabels(long userId);
+
+
+    }
+}
