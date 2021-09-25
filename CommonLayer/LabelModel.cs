a449d7fb89f4ab4575f0810cebe5da80907ccee2
Prabal <prabal760@gmail.com>
2021-09-25 10:59:09 +0530

    [Prabal] Added-Label Model

---
+++ CommonLayer/LabelModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+   public class LabelModel
+    {
+        public string LabelName { get; set; }
+
+   }
+}
